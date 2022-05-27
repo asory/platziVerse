46063d97c2ca0ef8197fb81226382cbcee15011f
Rosa Blanca <rosablanca.pg@gmail.com>
2022-05-27 00:11:39 -0400

    Initial commit

---
+++ Assets/Scripts/ghostFloor.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ghostFloor : MonoBehaviour
+{
+
+
+    [SerializeField] string playerTag = "Player";
+    [SerializeField] float disappearTime = 3;
+    [SerializeField] bool canReset;
+    [SerializeField] float restTime;
+    //[SerializeField] GameObject floor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+       
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+
+
+        if (other.transform.tag == playerTag)
+        {
+            print("OnTriggerEnter");
+
+            print(other.transform.tag);
+            Destroy(gameObject, disappearTime);
+
+        }
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# platziVerse
